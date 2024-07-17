8c394cd6241e78ebddfa1729d7e41d2b0f86c2d5
harshjhasd <harshjha191019972gmail.com>
2024-07-17 12:39:23 +0530

    Updated Code with text change in experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,11 +48,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled Frontend developer with 2+ years of experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, NextJs, and
-        Three.js. I have collaborated closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled Frontend developer with 2.5+ years of experience in
+        TypeScript and JavaScript, and expertise in frameworks like React,
+        NextJs, and Three.js. I have collaborated closely with clients to create
+        efficient, scalable, and user-friendly solutions that solve real-world
+        problems. Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Harsh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Developer with 2+ years of experience<br className='sm:block hidden' />
+            Developer with 2.5+ years of experience<br className='sm:block hidden' />
              in React , NextJs.
           </p>
         </div>
